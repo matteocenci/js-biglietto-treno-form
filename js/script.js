b739bfb8929552623655a664817d083e765b5252
matteocenci <matteocenci01@gmail.com>
2024-03-26 16:55:35 +0100

    feat: function and output works

--- js/script.js
+++ js/script.js
@@ -65,11 +65,51 @@ sendBtnElem.addEventListener("click", function(){
     // test
     // console.log("CLICK");
 
+    // DATI
+
     const ageInputElem = document.getElementById("age"); //object
     console.log(ageInputElem);
 
-    const ageValue = parseInt(ageInputElem.value);
+    const ageValue = parseInt(ageInputElem.value); //number
     console.log(ageValue, typeof ageValue);
+
+    const kmInputElem = document.getElementById("km-number"); //object
+    console.log(kmInputElem);
+
+    const kmValue = parseInt(kmInputElem.value); //number
+    console.log(kmValue, typeof kmValue);
+
+    // ESECUZIONE PROGRAMMA
+    const standardPrice = 0.21 * kmValue;
+    console.log(standardPrice);
+    let finalPrice = standardPrice 
+    let bgClass;
+    if (ageValue < 18) {
+        finalPrice = standardPrice * 0.80;
+        resultAge = "Avendo " + ageValue + " anni, può avere lo sconto del 20%";
+        bgClass = "minorenne";
+    } else if (ageValue > 69) {
+        finalPrice = standardPrice * 0.60;
+        resultAge = "Avendo " + ageValue + " anni, può avere lo sconto del 40%";
+        bgClass = "over";
+
+    } else {
+        finalPrice = standardPrice;
+        resultAge ="Avendo " + ageValue + " anni, non ti verrà applicato nessuno sconto"
+        bgClass = "maggiorenne;"
+    }
+    console.log(finalPrice);
+    
+    console.log(resultAge);
+    document.getElementById("finalprice").innerHTML = "Pagamento: " + (finalPrice)+"€";
+    document.getElementById("condition").innerHTML = (resultAge);
+
+    // `
+    //     <p class="${bgClass}!>${resultAge}</p>
+    // `;
+    
+    
+    
 })
 
 
